a527401ce3fd2137a979df322ebf457cb116a0ee
Levon Baghdasaryan <levonbaghdasarian@gmail.com>
2023-08-27 02:50:47 +0400

    user update

--- users/urls/user_urls.py
+++ users/urls/user_urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('', user_views.index, name='index'),
     path('profile/', user_views.profile, name='profile'),
+    path('update/', user_views.update, name='update'),
 ]
--- users/views/user_views.py
+++ users/views/user_views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -20,5 +21,23 @@ def index(request):
 @permission_classes([IsAuthenticated])
 def profile(request):
     user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update(request):
+    user = request.user
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password']:
+        user.password = make_password(data['password'])
+
+    user.save()
+
     serializer = UserSerializer(user)
     return Response(serializer.data)
